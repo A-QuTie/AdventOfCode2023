88b3281eb958b491b71f83c258ef1c8833e6c110
Anthony Tran <anthonytran1996@outlook.com.au>
2023-12-10 21:07:26 +1100

    completed day 02 part 1

--- day02/day02_part1.py
+++ day02/day02_part1.py
@@ -8,29 +8,41 @@ cube_colors["red"]: str = int(input("How many red cubes? "))
 cube_colors["green"]: str = int(input("How many green cubes? "))
 cube_colors["blue"]: str = int(input("How many blue cubes? "))
 
-def check_cubes(cubes: dict[str, int]):
-    for color in cube_colors:
-        for 
+def check_cubes(cubes: dict[str, int]) -> bool:
+    for color in ["red", "green", "blue"]:
+        for num_cubes in cubes[color]:
+            if num_cubes > cube_colors[color]:
+                return False
+    return True
 
+def init_cubes():
+    with open("day02input.txt", "r", encoding="utf-8") as f:
+        valid_games = [];
+        while line := f.readline().strip():
+            game, line = line.split(":")
+            sets = line.split(";")
+            
+            num_cubes = {}
+            for s in sets:
+                
+                cubes = s.split(",")
+                
+                for cube in cubes:
+                    
+                    num, color = cube.strip().split(" ")
+                    
+                    if color not in num_cubes:
+                        num_cubes[color] = [int(num)]
+                    else:
+                        num_cubes[color].append(int(num))
 
-with open("day02input.txt", "r", encoding="utf-8") as f:
-    valid_games = [];
-    while line := f.readline().strip():
-        game, line = line.split(":")
-        sets = line.split(";")
-        num_cubes = {}
-        for s in sets:
-            num, cube = cube.split()
-            if cube not in num_cubes:
-                num_cubes[cube] = int(num)
-            else:
-                num_cubes[cube] += int(num)
-        print(game, num_cubes)
-        if num_cubes["red"] > red or num_cubes["green"] > green or num_cubes["blue"] > blue:
-            continue
-        valid_games.append(game)
+            if check_cubes(num_cubes):
+                valid_games.append(int(game.split(" ")[1]))
+        print(sum(valid_games))
 
-    print(valid_games)
-
-        
+def main():
+    init_cubes()
+    
+if __name__ == "__main__":
+    main()
 
